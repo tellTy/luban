fa60792e77cba2ad62adb0f8e00c1f52cb2263b8
tangyan <1023788869>
2025-10-21 17:39:53 +0800

    1

--- gateway-service/src/main/java/com/example/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gateway/GatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication(exclude = {
@@ -12,8 +13,9 @@ import org.springframework.core.env.Environment;
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-        Environment env = SpringApplication.run(GatewayServiceApplication.class, args).getEnvironment();
-        System.out.println("spring.main.allow-bean-definition-overriding: " +
-                env.getProperty("spring.main.allow-bean-definition-overriding"));
+        ConfigurableApplicationContext context = SpringApplication.run(GatewayServiceApplication.class, args);
+        Environment env = context.getEnvironment();
+        // 打印关键配置
+        System.out.println("=== 配置值：" + env.getProperty("spring.main.allow-bean-definition-overriding"));
     }
 }
